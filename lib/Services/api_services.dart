b4ab901342057575c5ba97ce903d66060c4ee434
arafatappdeveloper <appdeveloperarafat@gmail.com>
2024-12-30 03:15:49 +0600

    api Link

---
+++ lib/Services/api_services.dart
@@ -0,0 +1,6 @@
+class ApiServices{
+  final String apiKey="";
+  final String forecastbaseUrl="http://api.weatherapi.com/v1/forecast.json";
+  final String searchUrl="http://api.weatherapi.com/v1//search.json";
+
+}
